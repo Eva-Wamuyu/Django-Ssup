c5984da64f07431244a191e325003a02e9af403a
Eva <rutheve.eva@gmail.com>
2022-06-19 07:36:11 +0300

    define forms

---
+++ hood/forms.py
@@ -0,0 +1,81 @@
+from random import choices
+from django import forms
+from .models import HoodUser,Business, Post
+
+
+class RegistrationForm(forms.ModelForm):
+  first_name = forms.CharField(max_length=120)
+  last_name = forms.CharField(max_length=120)
+  password = forms.CharField(max_length=255, widget=forms.PasswordInput())
+  class Meta:
+    model = HoodUser
+    fields = ['first_name', 'last_name', 'email', 'password','hood' ]
+
+    labels = {
+      'first_name': 'First Name',
+      'last_name': 'Last Name',
+      'email': 'Email Address',
+      'password': 'A Strong Password'
+    }
+
+class LoginForm(forms.ModelForm):
+  password = forms.CharField(max_length=255, widget=forms.PasswordInput())
+  email = forms.CharField(max_length=255, widget=forms.EmailInput())
+  class Meta:
+    model = HoodUser
+    fields = ['email', 'password']
+
+    labels = {
+      'email': 'Your Email',
+      'password': 'Password',
+    
+    }
+
+
+
+
+class BusinessForm(forms.ModelForm):
+  class Meta:
+    model = Business
+    exclude = ['user',]
+
+    labels = {
+      'name': 'Business Name',
+      'hood': 'Business Locality',
+      'biz_type':'What Does it Deal with',
+      'avatar': 'A business Photo can be handy'
+    }
+
+
+class AddPost(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ['post']
+
+
+  
+class UpdateProfile(forms.ModelForm):
+  class Meta:
+    model = HoodUser
+    fields = ['avatar','msg','hood']
+
+    labels = {
+      'avatar': 'An Image',
+      'msg': 'Message/Bio',
+      'hood': 'Where Do You Live'
+    }
+
+  
+
+
+
+
+  
+
+
+    
+   
+
+  
+
+
